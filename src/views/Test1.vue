dcbee70a02abb86d424a5e541e2b6df6f5549759
duanmingqiang <964178976@qq.com>
2022-05-21 20:06:15 +0800

    增加截图例子使用

--- src/views/Test1.vue
+++ src/views/Test1.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
       hello test1
+      <el-table :data="tableData" id="table">
+        <el-table-column prop="test"></el-table-column>
+      </el-table>
+      <el-button @click="handleClick">
+        截图
+      </el-button>
   </div>
 </template>
 
 <script>
+import html2canvas from 'html2canvas'
 export default {
-
+  data() {
+    return {
+      tableData: [
+        {
+          test: 666
+        }
+      ]
+    }
+  },
+  methods: {
+    handleClick() {
+      html2canvas(document.getElementById('table')).then((canvas) => {
+        console.log(canvas.toDataURL())
+        window.aaa = canvas
+      })
+    }
+  }
 }
 </script>
 
